6e136e2dd7930a5eba02cde0cbaf75f2d38e9e7e
Faris <madebyais@gmail.com>
2021-07-26 05:40:26 +0700

    Add set text alignment to product thumbnail 1

--- components/e-commerce/product-thumbnail-1.tsx
+++ components/e-commerce/product-thumbnail-1.tsx
@@ -1,7 +1,8 @@
-import { Image } from "antd"
-import image from "next/image"
+import Image from "next/image"
 import Link from "next/link"
 import { FC } from "react"
+
+type textAlign = 'left' | 'center' | 'right'
 
 interface IProductThumbnail1Props {
   readonly imageUrl?: string
@@ -11,26 +12,49 @@ interface IProductThumbnail1Props {
   readonly price: number
   readonly discountedPrice?: number
   readonly href: string
+  readonly textAlign?: textAlign
 }
 
-const ProductThumbnail1: FC<IProductThumbnail1Props> = ({ imageUrl = '', title, category = '', currency = 'IDR', price = 0, discountedPrice = 0, href }) => (
-  <Link href={href} passHref>
-    <div className={`cursor-pointer`}>
-      {imageUrl.length > 0 && <Image src={imageUrl} alt={`image`} preview={false} />}
-      <div className={`font-bold`}>{title}</div>
-      {category.length > 0 && <div>{category}</div>}
-      <div className={`flex justify-start items-center`}>
-        {discountedPrice > 0 ? (
-          <>
-            <div className={`font-bold text-red-500 mr-2`}>{currency} {discountedPrice.toLocaleString()}</div>
-            <div className={`text-xs line-through text-gray-500`}>{currency} {price.toLocaleString()}</div>
-          </>
-        ) : (
-          <div className={`font-bold`}>{currency} {price.toLocaleString()}</div>
-        )}
+const ProductThumbnail1: FC<IProductThumbnail1Props> = ({ imageUrl = '', title, category = '', currency = 'IDR', price = 0, discountedPrice = 0, href, textAlign = 'left' }) => {
+  const getTextAlign = (type: string, align: string) => {
+    if (type === 'text') return `text-${align}`
+    if (type === 'price') {
+      let textAlignment: string
+      switch (align.toLowerCase()) {
+        case 'left':
+          textAlignment = 'justify-start'
+          break
+        case 'center':
+          textAlignment = 'justify-center'
+          break
+        case 'right':
+          textAlignment = 'justify-end'
+          break
+        default:
+          textAlignment = 'justify-start'
+      }
+      return textAlignment
+    }
+  }
+  return (
+    <Link href={href} passHref>
+      <div className={`cursor-pointer ${getTextAlign('text', textAlign)}`}>
+        {imageUrl.length > 0 && <Image src={imageUrl} alt={`image`} width={400} height={600} layout={`responsive`} />}
+        <div className={`font-bold mt-2`}>{title}</div>
+        {category.length > 0 && <div>{category}</div>}
+        <div className={`flex  ${getTextAlign('price', textAlign)} items-center`}>
+          {discountedPrice > 0 ? (
+            <>
+              <div className={`font-bold text-red-500 mr-2`}>{currency} {discountedPrice.toLocaleString()}</div>
+              <div className={`text-xs line-through text-gray-500`}>{currency} {price.toLocaleString()}</div>
+            </>
+          ) : (
+            <div className={`font-bold`}>{currency} {price.toLocaleString()}</div>
+          )}
+        </div>
       </div>
-    </div>
-  </Link>
-)
+    </Link>
+  )
+}
 
 export default ProductThumbnail1
--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   reactStrictMode: true,
 
+  images: {
+    domains: ['placeimg.com']
+  },
+
   env: {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
